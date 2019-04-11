10945fbefc9e9d42395e092102a58e2eb6d2aee1
Uturi <41833265+Uturi@users.noreply.github.com>
2019-04-11 22:57:41 +0900

    Stock

--- src/com/kitri/pos/MainFrame.java
+++ src/com/kitri/pos/MainFrame.java
@@ -1,36 +1,49 @@
 package com.kitri.pos;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
-
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableColumnModel;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
-import java.awt.Font;
 import javax.swing.SwingConstants;
-import java.awt.Color;
 import javax.swing.JButton;
-import java.awt.CardLayout;
+
 import java.awt.event.ActionListener;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 
-public class MainFrame extends JFrame implements ActionListener {
+public class MainFrame extends JFrame implements ActionListener{
 
+	StockPopupIn stockpopupin = new StockPopupIn();
+	StockPopupChg stockpopupchg = new StockPopupChg();
+	StockPopupSearch stockpopupsearch = new StockPopupSearch();
+	StockBtn stockbtn;
+	StockMonitor stockmonitor;
+	CardLayout monitor;
+	CardLayout btn;
+	
 	private JPanel contentPane;
 	private JTextField notice;
-	
-	ForcePos frame;
-	Administrator adframe = new Administrator();
+	private JPanel pStatusBar;
+	private JLabel titleLabel;
+	private JLabel dateLabel;
+	private JPanel pMainBtn;
+	private JLabel idLabel;
+	private JButton mBtnInven;
+	private JButton mBtnSale;
+	private JButton mBtnCalc;
+	private JButton mBtnStat;
+	private JButton mBtnAccount;
+	private JPanel pMonitor;
+	private JPanel pFBtn;
 
 	/**
 	 * Launch the application.
@@ -41,8 +54,6 @@ public class MainFrame extends JFrame implements ActionListener {
 				try {
 					MainFrame frame = new MainFrame();
 					frame.setVisible(true);
-//					Administrator frame2 = new Administrator();
-//					frame2.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -53,40 +64,37 @@ public class MainFrame extends JFrame implements ActionListener {
 	/**
 	 * Create the frame.
 	 */
-
-	//계정창 보여주세요~
-	public void showFrameTest() {
-//		frame1.setVisible(true);
-//		frame.dispose();
-		this.setVisible(false);
-		adframe.setVisible(true);
-	}
-
 	public MainFrame() {
+		monitor = new CardLayout();
+		stockmonitor = new StockMonitor();
+		stockbtn = new StockBtn();
+		btn = new CardLayout();
+		
 		setFont(new Font("맑은 고딕", Font.BOLD, 20));
 		setTitle("Force.pos");
-		setAlwaysOnTop(true);
+//		setAlwaysOnTop(true);
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(10, 15, 1326, 753);
+		setBounds(100, 100, 1326, 753);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-
-		JPanel pStatusBar = new JPanel();
+		
+		pStatusBar = new JPanel();
 		pStatusBar.setBackground(new Color(0, 0, 128));
-		pStatusBar.setBounds(0, 0, 1308, 51);
+		pStatusBar.setBounds(0, 0, 1320, 51);
 		contentPane.add(pStatusBar);
 		pStatusBar.setLayout(null);
-
-		JLabel titleLabel = new JLabel("Force. pos");
+		
+		titleLabel = new JLabel("Force. pos");
 		titleLabel.setForeground(Color.WHITE);
 		titleLabel.setBackground(new Color(0, 0, 128));
 		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		titleLabel.setFont(new Font("맑은 고딕", Font.BOLD, 25));
 		titleLabel.setBounds(14, 8, 241, 31);
 		pStatusBar.add(titleLabel);
-
+		
 		notice = new JTextField();
 		notice.setFont(new Font("맑은 고딕", Font.PLAIN, 20));
 		notice.setText("\uC0C1\uD488\uBA85(..)\uB294 \uC720\uD1B5\uAE30\uD55C\uC774 \uC9C0\uB0AC\uC2B5\uB2C8\uB2E4.");
@@ -94,129 +102,138 @@ public class MainFrame extends JFrame implements ActionListener {
 		notice.setBounds(258, 8, 726, 31);
 		pStatusBar.add(notice);
 		notice.setColumns(10);
-
-		JLabel dateLabel = new JLabel("2019-04-01 \uC624\uD6C4 5:01");
+		
+		dateLabel = new JLabel("2019-04-01 \uC624\uD6C4 5:01");
 		dateLabel.setBackground(new Color(0, 0, 128));
 		dateLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		dateLabel.setForeground(new Color(255, 255, 255));
 		dateLabel.setFont(new Font("맑은 고딕", Font.PLAIN, 20));
 		dateLabel.setBounds(1016, 8, 278, 31);
 		pStatusBar.add(dateLabel);
-
-		JPanel pMainBtn = new JPanel();
+		
+		pMainBtn = new JPanel();
 		pMainBtn.setBackground(new Color(255, 255, 255));
-		pMainBtn.setBounds(0, 585, 1144, 120);
+		pMainBtn.setBounds(0, 600, 1157, 125);
 		contentPane.add(pMainBtn);
 		pMainBtn.setLayout(null);
-
-		JLabel idLabel = new JLabel("\uAD00\uB9AC\uC790");
-		idLabel.setBackground(new Color(105, 105, 105));
+		
+		idLabel = new JLabel("\uAD00\uB9AC\uC790");
+		idLabel.setForeground(Color.BLACK);
+		idLabel.setBackground(new java.awt.Color(105, 105, 105));
 		idLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		idLabel.setFont(new Font("맑은 고딕", Font.BOLD, 20));
-		idLabel.setBounds(860, 0, 201, 120);
+		idLabel.setBounds(860, 0, 201, 125);
+		idLabel.setOpaque(true);
 		pMainBtn.add(idLabel);
-
-		JButton mBtnInven = new JButton("\uC7AC\uACE0");
+		
+		mBtnInven = new JButton("\uC7AC\uACE0");
 		mBtnInven.setBackground(new Color(28, 94, 94));
 		mBtnInven.setForeground(new Color(255, 255, 255));
 		mBtnInven.setFont(new Font("맑은 고딕", Font.BOLD, 20));
-		mBtnInven.setBounds(0, 0, 157, 120);
+		mBtnInven.setBounds(0, 0, 157, 125);
 		pMainBtn.add(mBtnInven);
-
-		JButton mBtnSale = new JButton("\uD310\uB9E4");
+		
+		mBtnSale = new JButton("\uD310\uB9E4");
 		mBtnSale.setBackground(new Color(99, 166, 166));
 		mBtnSale.setFont(new Font("맑은 고딕", Font.BOLD, 20));
 		mBtnSale.setForeground(new Color(255, 255, 255));
-		mBtnSale.setBounds(156, 0, 157, 120);
+		mBtnSale.setBounds(156, 0, 157, 125);
 		pMainBtn.add(mBtnSale);
-
-		JButton mBtnCalc = new JButton("\uC815\uC0B0");
+		
+		mBtnCalc = new JButton("\uC815\uC0B0");
 		mBtnCalc.setBackground(new Color(28, 94, 94));
 		mBtnCalc.setForeground(new Color(255, 255, 255));
 		mBtnCalc.setFont(new Font("맑은 고딕", Font.BOLD, 20));
-		mBtnCalc.setBounds(313, 0, 157, 120);
+		mBtnCalc.setBounds(313, 0, 157, 125);
 		pMainBtn.add(mBtnCalc);
-
-		JButton mBtnStat = new JButton("\uD1B5\uACC4");
+		
+		mBtnStat = new JButton("\uD1B5\uACC4");
 		mBtnStat.setBackground(new Color(99, 166, 166));
 		mBtnStat.setFont(new Font("맑은 고딕", Font.BOLD, 20));
 		mBtnStat.setForeground(new Color(255, 255, 255));
-		mBtnStat.setBounds(470, 0, 157, 120);
+		mBtnStat.setBounds(470, 0, 157, 125);
 		pMainBtn.add(mBtnStat);
-
-		JButton mBtnAccount = new JButton("\uACC4\uC815");
+		
+		mBtnAccount = new JButton("\uACC4\uC815");
 		mBtnAccount.setBackground(new Color(28, 94, 94));
 		mBtnAccount.setForeground(new Color(255, 255, 255));
 		mBtnAccount.setFont(new Font("맑은 고딕", Font.BOLD, 20));
-		mBtnAccount.setBounds(626, 0, 157, 120);
+		mBtnAccount.setBounds(626, 0, 157, 125);
 		pMainBtn.add(mBtnAccount);
-
-		JPanel pSellFunction = new JPanel();
-		pSellFunction.setBackground(new Color(0, 0, 128));
-		pSellFunction.setBounds(1144, 50, 164, 655);
-		contentPane.add(pSellFunction);
-		pSellFunction.setLayout(null);
-
-		JButton sBtnCustomer = new JButton("\uC7AC\uACE0\uC870\uD68C");
-		sBtnCustomer.setForeground(new Color(255, 255, 255));
-		sBtnCustomer.setBackground(new Color(0, 0, 128));
-		sBtnCustomer.setFont(new Font("맑은 고딕", Font.BOLD, 20));
-		sBtnCustomer.setBounds(0, 0, 164, 120);
-		pSellFunction.add(sBtnCustomer);
-
-		JButton sBtnDisuse = new JButton("\uC785\uCD9C\uACE0");
-		sBtnDisuse.setBackground(new Color(100, 149, 237));
-		sBtnDisuse.setForeground(new Color(255, 255, 255));
-		sBtnDisuse.setFont(new Font("맑은 고딕", Font.BOLD, 20));
-		sBtnDisuse.setBounds(0, 130, 164, 120);
-		pSellFunction.add(sBtnDisuse);
-
-		JButton sBtnPdInput = new JButton("\uC7AC\uACE0\uB4F1\uB85D");
-		sBtnPdInput.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		sBtnPdInput.setBackground(new Color(0, 0, 128));
-		sBtnPdInput.setForeground(new Color(255, 255, 255));
-		sBtnPdInput.setFont(new Font("맑은 고딕", Font.BOLD, 20));
-		sBtnPdInput.setBounds(0, 260, 164, 120);
-		pSellFunction.add(sBtnPdInput);
-
-		JButton sBtnPdChange = new JButton("\uC7AC\uACE0\uC218\uC815");
-		sBtnPdChange.setBackground(new Color(100, 149, 237));
-		sBtnPdChange.setForeground(new Color(255, 255, 255));
-		sBtnPdChange.setFont(new Font("맑은 고딕", Font.BOLD, 20));
-		sBtnPdChange.setBounds(0, 390, 164, 120);
-		pSellFunction.add(sBtnPdChange);
-
-		JButton sBtnPdCancel = new JButton("\uC7AC\uACE0\uC0AD\uC81C");
-		sBtnPdCancel.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		sBtnPdCancel.setBackground(new Color(0, 0, 128));
-		sBtnPdCancel.setForeground(new Color(255, 255, 255));
-		sBtnPdCancel.setFont(new Font("맑은 고딕", Font.BOLD, 20));
-		sBtnPdCancel.setBounds(0, 520, 164, 120);
-		pSellFunction.add(sBtnPdCancel);
-
-		JPanel pMonitor = new JPanel();
+		
+		pMonitor = new JPanel();
 		pMonitor.setBackground(new Color(255, 255, 255));
-		pMonitor.setBounds(0, 50, 1144, 535);
+		pMonitor.setBounds(0, 50, 1157, 552);
 		contentPane.add(pMonitor);
-		pMonitor.setLayout(new CardLayout(0, 0));
+		pMonitor.setLayout(monitor);
+//		모니터패널에 카드레이아웃주고 재고화면 패널 객체생성하여 추가
+		pMonitor.add(stockmonitor, "Stock");
 		
 		
+		pFBtn = new JPanel();
+		pFBtn.setBackground(Color.BLUE);
+		pFBtn.setBounds(1156, 50, 164, 675);
+		contentPane.add(pFBtn);
+		pFBtn.setLayout(btn);
+//		기능버튼패널 카드 재고기능버튼 패널 객체생성후 추가
+		pFBtn.add(stockbtn, "Stockbtn");
+		
+		
+		mBtnInven.addActionListener(this);
+		mBtnSale.addActionListener(this);
+		mBtnCalc.addActionListener(this);
+		mBtnStat.addActionListener(this);
 		mBtnAccount.addActionListener(this);
-	}
+		
+		stockbtn.stockSearch.addActionListener(this);
+		stockbtn.stockIn.addActionListener(this);
+		stockbtn.stockChg.addActionListener(this);
+		stockbtn.stockRefresh.addActionListener(this);
+		
+		
+	}//메인프레임메소드 끝
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		Object ob = e.getActionCommand();
-
-		if (ob.equals("계정")) {
-			showFrameTest();
+		Object ob = e.getSource();
+		if(ob==mBtnInven) {
+			monitor.show(pMonitor, "Stock");
+			btn.show(pFBtn, "Stockbtn");
+		} else if(ob==stockbtn.stockSearch) {
+			stockpopupsearch.setVisible(true);
+		} else if(ob==stockbtn.stockChg) {
+//			String tmp = stockmonitor.getText();
+//			if (to.isEmpty()) {
+//				JOptionPane.showMessageDialog(chat, "대상자를 선택하세요.", "대상자오류", JOptionPane.ERROR_MESSAGE);
+//				return;
+//			}
+//
+//			if (to.equals(myid)) {
+//				JOptionPane.showMessageDialog(chat, "자신에게 쪽지를???", "대상자오류", JOptionPane.INFORMATION_MESSAGE);
+//				paper.letter.setText("");
+//				return;
+//			}
+			
+			stockpopupchg.setVisible(true);
+		} else if(ob==stockbtn.stockIn) {
+			stockpopupin.setVisible(true);
+		} else if(ob==stockbtn.stockRefresh) {
+			JOptionPane.showMessageDialog(this, "비어있는 재고를 삭제 하시겠습니까?", "새로고침", JOptionPane.OK_CANCEL_OPTION);
+			
 		}
+		
+	}//액션퍼폼끝
+	
+	// <테이블 내용 가운데 정렬> 메소드
+		public static void tableCellCenter(JTable t) {
+			DefaultTableCellRenderer dtcr = new DefaultTableCellRenderer(); // 디폴트테이블셀렌더러를 생성
+			dtcr.setHorizontalAlignment(SwingConstants.CENTER); // 렌더러의 가로정렬을 CENTER로
 
-	}
-}
+			TableColumnModel tcm = t.getColumnModel(); // 정렬할 테이블의 컬럼모델을 가져옴
+
+			// 전체 열에 지정
+			for (int i = 0; i < tcm.getColumnCount(); i++) {
+				tcm.getColumn(i).setCellRenderer(dtcr);
+			}
+		}
+}//클래스 끝
